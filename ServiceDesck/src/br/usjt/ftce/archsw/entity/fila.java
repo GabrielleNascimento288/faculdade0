74c55d611dfd8fc8e3f1674ddf2e672e16e6b561
Gabrielle DKP <Gabrielle DKP@10.71.139.213>
2018-02-21 22:21:40 -0300

    Comentario

---
+++ ServiceDesck/src/br/usjt/ftce/archsw/entity/fila.java
@@ -0,0 +1,21 @@
+package br.usjt.ftce.archsw.entity;
+
+public class fila {
+
+		private int id; 
+		private String nome;
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+}
